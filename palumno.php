7781fbc890bf882aca2195d93afcaf0e3cf7d4b2
Everardo <ebecerrild@gmail.com>
2019-12-04 20:32:03 -0600

    resuelto conflicto

--- palumno.php
+++ palumno.php
@@ -3,7 +3,7 @@ session_start();
 
 require 'database.php';
 
-$records = $conn-> prepare('SELECT first_name FROM users WHERE user_id=:id');
+$records = $conn-> prepare('SELECT first_name, last_name, username, email FROM users WHERE user_id=:id');
 $records->bindParam(':id', $_SESSION['user_id']);
 $records->execute();
 $results = $records->fetch();
@@ -30,29 +30,38 @@ if(count($results) > 0){
 	<div>
 		<ul>
 			<li><img src="img/logo.png" alt="logo" width = "" height="20"></li>
-			<li><a href="index.php">Cerrar sesión</a></li>
+			<li><a href="logout.php">Cerrar sesión</a></li>
 			<li><a href="palumno.php">Home</a></li>
 			<li><a href="pin.php">Nuevo Juego</a></li>
 		</ul>
 	</div>
-	<br><br>
-	
+	<h1 style="background-color:hsl(0, 100%, 0%);">Bienvenido <?= $user['first_name']?>, </h1> 
 	<div id="cuerpo"> 
 		<div id="lateral"> 
 			<img src="img/profile.png">
-			<p>Bio</p>
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin rutrum ligula non purus gravida volutpat. Donec nec dui sit amet tortor dignissim efficitur.  Duis a lectus non ex aliquet lacinia a nec libero. Proin hendrerit tempor eros, eu ornare lacus gravida sed. Phasellus sed condimentum tellus. Ut eleifend justo diam, at blandit velit finibus sed. Cras lacinia lectus a mattis iaculis. </p>
 		</div> 
 		<div id="principal"> 
+
 
 			<br><br>
 			<h1>Bienvenido <?= $user['first_name']?>, </h1> 
 			<br><br><br>
+		
 			
-			<h2>Informacion personal</h2>
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin rutrum ligula non purus gravida volutpat. Donec nec dui sit amet tortor dignissim efficitur. Duis a lectus non ex aliquet lacinia a nec libero. Proin hendrerit tempor eros, eu ornare lacus gravida sed. Phasellus sed condimentum tellus. Ut eleifend justo diam, at blandit velit finibus sed. Cras lacinia lectus a mattis iaculis. Duis odio massa, rhoncus et lectus at, interdum finibus risus. Donec ac sapien efficitur, bibendum tortor nec, pellentesque orci. Quisque ut lobortis lacus. Sed vel massa eu augue pretium consequat. Ut mattis nibh eget ultrices sollicitudin. Nulla semper, justo sed consectetur scelerisque, dolor eros tincidunt magna, vitae gravida quam enim sit amet risus. Donec posuere diam vel quam consectetur iaculis.</p>
+
+
+			<br><br><br><br><br><br><br><br>
+			
+			
+			<h2><font color="black">Información personal</font></h2>
+			<p>Nombre: <?= $user['first_name']?> <?= $user['last_name']?></p>
+			<p>Username: <?= $user['username']?> </p>
+			<p>Email: <?= $user['email']?> </p>
 
 		</div> 
+		<div><br>
+			<h2><font color="black">Logros</font></h2>
+		</div>
 	</div> 
  
 	
